70cd62255c660837a19453e55eae245e77fa4ef5
Hariharan Srinath <srinathh@gmail.com>
2017-03-11 12:23:52 +0530

    not working well. jumping

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+//go:generate go-bindata assets/
+
 import (
 	"bytes"
 	"flag"
@@ -18,10 +20,12 @@ const (
 	scrH    = "scrH"
 	boxW    = "boxW"
 	boxH    = "boxH"
+	deltaX  = "deltaX"
+	deltaY  = "deltaY"
 	evtType = "evtType"
 )
 
-var intParams = []string{curX, curY, scrW, scrH, boxW, boxH}
+var intParams = []string{curX, curY, scrW, scrH, boxW, boxH, deltaX, deltaY}
 
 func serveAsset(w http.ResponseWriter, r *http.Request) {
 	urlpath := r.URL.String()
@@ -73,65 +77,6 @@ func main() {
 	flag.IntVar(&yadjust, "yadjust", 0, "radjust")
 	flag.Parse()
 
-	/*
-		http.HandleFunc("/mousedata", func(w http.ResponseWriter, r *http.Request) {
-
-
-				log.Println(r.FormValue("XPos"), r.FormValue("YPos"), r.FormValue("Width"), r.FormValue("Height"))
-				xpos, err := strconv.Atoi(r.FormValue("XPos"))
-				if err != nil {
-					log.Printf("Error parsing XPos:%s", r.FormValue("XPos"))
-					return
-				}
-
-				ypos, err := strconv.Atoi(r.FormValue("YPos"))
-				if err != nil {
-					log.Printf("Error parsing YPos: %s", r.FormValue("YPos"))
-					return
-				}
-
-				width, err := strconv.Atoi(r.FormValue("Width"))
-				if err != nil {
-					log.Printf("Error parsing Width: %s", r.FormValue("Width"))
-					return
-				}
-				height, err := strconv.Atoi(r.FormValue("Height"))
-				if err != nil {
-					log.Printf("Error parsing Height: %s", r.FormValue("Height"))
-					return
-				}
-
-				xpos = xpos + xadjust
-				if xpos < 0 {
-					xpos = 0
-				}
-				if xpos > width {
-					xpos = width
-				}
-
-				ypos = ypos + yadjust
-				if ypos < 0 {
-					ypos = 0
-				}
-				if ypos > height {
-					ypos = height
-				}
-
-				x := int(float64(xpos) / float64(width) * float64(scrWidth))
-				y := int(float64(ypos) / float64(height) * float64(scrHeight))
-				switch r.FormValue("EvtType") {
-				case "pan":
-					cmd := exec.Command("xdotool", "mousemove", strconv.Itoa(x), strconv.Itoa(y))
-					cmd.Run()
-				case "tap":
-					cmd := exec.Command("xdotool", "click", "1")
-					cmd.Run()
-				default:
-					log.Printf("Error: Unknown command %s", r.FormValue("EvtType"))
-				}
-
-		})*/
-
 	http.HandleFunc("/mousedata", func(w http.ResponseWriter, r *http.Request) {
 		params, err := parseNums(r, intParams)
 		if err != nil {
@@ -148,28 +93,28 @@ func main() {
 			boxTop := (params[scrH] - params[boxH]) / 2
 			boxBtm := params[scrH] - boxTop
 
-			if params[curX] < boxLeft {
-				params[curX] = 0
+			// if any inputs outside the bounding box, ignore
+			if params[curX] < boxLeft || params[curX] > boxRight || params[curY] < boxTop || params[curY] > boxBtm {
+				break
 			}
 
-			if params[curX] > boxRight {
-				params[curX] = boxRight
+			if params[deltaX] < params[boxW]/10 {
+				params[deltaX] = 0
 			}
 
-			if params[curY] < boxTop {
-				params[curY] = boxTop
+			if params[deltaY] < params[boxH]/10 {
+				params[deltaY] = 0
 			}
 
-			if params[curY] > boxBtm {
-				params[curY] = boxBtm
-			}
+			xPer := float32(params[deltaX]) / float32(params[boxW])
+			yPer := float32(params[deltaY]) / float32(params[boxH])
 
-			xPer := float32(params[curX]-boxLeft) / float32(params[boxW])
-			yPer := float32(params[curY]-boxTop) / float32(params[boxH])
+			x := int(xPer * float32(displayWidth) * 0.2)
+			y := int(yPer * float32(displayHeight) * 0.2)
 
-			x := int(xPer * float32(displayWidth))
-			y := int(yPer * float32(displayHeight))
-			exec.Command("xdotool", "mousemove", strconv.Itoa(x), strconv.Itoa(y)).Run()
+			log.Printf("xPer:%.2f yPer:%.2f x:%d y:%d", xPer, yPer, x, y)
+
+			exec.Command("xdotool", "mousemove_relative", "--", strconv.Itoa(x), strconv.Itoa(y)).Run()
 		case "tap":
 			exec.Command("xdotool", "click", "1").Run()
 		default:
